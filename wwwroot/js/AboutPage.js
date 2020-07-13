bdb83d349c74a8067061e19a0479396f0353254b
Dan Foster <dwfoster45@btinternet.com>
2020-07-13 23:25:44 +0100

    First JS added (about page read more) additions to both about page and home

---
+++ wwwroot/js/AboutPage.js
@@ -0,0 +1,51 @@
+﻿
+$(document).ready(function () {
+
+});
+
+$('#learnMoreButton').click(function ()
+{
+    $('#learnMoreJumbo').css("height", "302px");
+    $('#learnMoreJumbo').css("transition-duration", "0.5s");
+    $('#learnMoreJumbo').css("height", "400px");
+
+
+
+    $('#learnMorePara').css("visibility", "visible");
+    $('#learnMorePara').css("height", "150px;");
+
+    $(this).toggle();
+}); 
+
+    //if (toggle === undefined) {
+    //    alert('initial');
+    //    toggle = 1;
+    //}
+
+    //if (toggle = 1)
+    //{
+
+    //    $('#learnMoreJumbo').css("height", "400px");
+
+    //    $('#learnMoreButton').css("float", "left");
+
+    //    $('#learnMorePara').css("visibility", "visible");
+
+    //    alert('1');
+
+    //    toggle = 2;
+    //}
+    //else if (toggle = 2)
+    //{
+
+    //    $('#learnMoreJumbo').css("height", "200px");
+
+    //    $('#learnMoreButton').css("float", "left");
+
+    //    $('#learnMorePara').css("visibility", "hidden");
+
+    //    alert('2');
+
+    //    toggle = 1;
+    //}
+    
