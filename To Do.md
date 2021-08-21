b3f1bac9a34a398e6a2832ccf2deb27f3a3cbeec
Dan Foster <34134879+Spookems@users.noreply.github.com>
2021-08-21 20:35:32 +0100

    NavBar css spacing increase, partial view for login reintroduced

--- To Do.md
+++ To Do.md
@@ -1,12 +1,22 @@
+
+
 # StoreSite
 
 	High Priority
 	
-	- Edit page exists but cant be reached, needs fixing
-	- Add in add / delete functions
 	- Format backend to implement repository / unit of work pattern
+	- Change Navigation to not look shite
+	- Get Filtering working
 
 	Low Priority
 
 	- Remove style and add into CSS
 	- Front end tweaks to scale fine at differnt sizes
+
+	General Tech Changes
+
+	- Update to .Net5
+	- Move Connection strings and Config info into appSettings
+	- Link to cloud version on Azure
+	- Rename Interfaces and add in Transients
+	- Add in reigons to controller and models
--- wwwroot/js/NavBar.js
+++ wwwroot/js/NavBar.js
@@ -13,3 +13,35 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+
+
+var TIMEOUT = 12000;
+
+var interval = setInterval(handleNext, TIMEOUT);
+
+function handleNext() {
+
+    var $radios = $('input[class*="slide-radio"]');
+    var $activeRadio = $('input[class*="slide-radio"]:checked');
+
+    var currentIndex = $activeRadio.index();
+    var radiosLength = $radios.length;
+
+    $radios
+        .attr('checked', false);
+
+
+    if (currentIndex >= radiosLength - 1) {
+
+        $radios
+            .first()
+            .attr('checked', true);
+
+    } else {
+
+        $activeRadio
+            .next('input[class*="slide-radio"]')
+            .attr('checked', true);
+
+    }
+}
